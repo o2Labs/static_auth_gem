6994bd85353dd7df4aa2b8e49d18fe3dce795f62
otaiga <karl.turner5@gmail.com>
2016-02-26 13:41:43 +0000

    update rspec syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,6 @@ APP_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 Dir[File.join(APP_ROOT, 'spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
-  config.color_enabled = true
-  config.formatter     = 'documentation'
+  config.color = true
+  config.formatter = 'documentation'
 end
